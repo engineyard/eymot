ef496fb8997b8d49440295ce264e3c7204efeeb6
Danish Khan <dankhan28@gmail.com>
2012-01-24 15:50:34 -0800

    changed data to search and added bundle exec to shell out of eysearch

--- models/search.rb
+++ models/search.rb
@@ -4,4 +4,18 @@ class Search
   # property <name>, <type>
   property :id, Serial
   
+  def eysearch(search)
+    query = search.join(' ')
+    say "searching for #{query}.."
+    result=`bundle exec eysearch --extend #{query} 2>&1`
+    regex = /tm\d+-s0+\d+/
+    result.to_s.each do |r|
+      if regex.match(r)
+        vm = regex.match(r)
+        output = HTTParty.get("http://ec2-50-16-52-4.compute-1.amazonaws.com/vm/#{vm}.json")
+        hash = output.to_hash
+        puts "#{hash["name"]} has RAM:#{hash["ram"]} and DISK:#{hash["disk"]}"
+      end
+    end
+  end
 end
